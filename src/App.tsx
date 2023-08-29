1b86a1ea16b93a8ec37bea891afd93266b74fc08
Koda777 <alexandre.decobert@epitech.eu>
2023-08-29 19:32:08 +0200

    [ADD] repo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: black;
+`;
+
+function App() {
+  return <Container></Container>;
+}
+
+export default App;
