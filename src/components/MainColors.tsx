e372038cb6300b2cf9277f063814c1a06c318259
Koda <alexandre.decobert@epitech.eu>
2023-09-18 13:58:20 +0200

    [ADD] apply colored border on double click

--- src/components/MainColors.tsx
+++ src/components/MainColors.tsx
@@ -2,6 +2,56 @@ import { useEffect, useState } from "react"
 import styled from "styled-components"
 import { invoke } from "@tauri-apps/api/tauri"
 
+const MainColors = () => {
+  const [response, setResponse] = useState("#fff")
+  const [locked, setLocked] = useState<boolean[]>([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data: any = await invoke("get_colors")
+        setResponse(JSON.parse(data))
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchData()
+  })
+
+  const handleDoubleClick = (index: number) => {
+    navigator.clipboard.writeText(response[index])
+  }
+
+  const handleLock = (index: number) => {
+    const newLocked = [...locked]
+    newLocked[index] = !newLocked[index]
+    setLocked(newLocked)
+  }
+
+  return (
+    <Container>
+      {[...Array(5)].map((index, i) => {
+        return (
+          <Square
+            key={`${index} - ${i}`}
+            color={response[i]}
+            onClick={() => handleClick(i)}
+            onDoubleClick={() => handleLock(i)}
+            borderColor={locked[i] ? "white" : "transparent"}
+          />
+        )
+      })}
+    </Container>
+  )
+}
+
+export default MainColors
+
+interface SquareProps {
+  color: string
+  borderColor: string
+}
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -11,39 +61,14 @@ const Container = styled.div`
   align-items: center;
 `
 
-const Square = styled.div`
+const Square: React.FC<SquareProps> = styled.div`
   height: 60px;
   width: 60px;
   border-radius: 5px;
   background: ${(props) => props.color};
+  border: 2px solid ${(props) => props.borderColor};
+
   &:hover {
     border: 2px solid white;
   }
 `
-
-const MainColors = () => {
-  const [response, setResponse] = useState("#fff")
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data: any = await invoke("get_colors")
-        console.log("colors is ", data)
-        setResponse(JSON.parse(data))
-      } catch (e) {
-        console.log(e)
-      }
-    }
-    fetchData()
-  })
-
-  return (
-    <Container>
-      {[...Array(5)].map((index, i) => {
-        return <Square key={`${index} - ${i}`} color={response[i]} />
-      })}
-    </Container>
-  )
-}
-
-export default MainColors
